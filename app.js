47d0c795e29a3a74222562fbb422c54cc31a9234
Moufid Ali <you@example.com>
2023-01-18 10:54:46 +0100

    new commit add ejs template for days

--- app.js
+++ app.js
@@ -7,9 +7,39 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 
 app.get("/",function (req ,res) {
-    res.send("Hello it the todolist");
+    var today = new Date();
+    var todayDay = today.getDay();
+    var day = "";
+    if (todayDay == 6 ) {
+        day = "Saturday";
+    } 
+    if  (todayDay == 0 ) {
+        day = "Sunday";
+    } 
+    if (todayDay == 1 ) {
+        day = "Lundi";
+    } 
+    if  (todayDay == 2 ) {
+        day = "Mardi";
+    } 
+    if  (todayDay == 3 ) {
+        day = "Mercredi";
+    } 
+    if  (todayDay == 4 ) {
+        day = "jeudi";
+    } 
+    if  (todayDay == 5 ) {
+        day = "vendredi";
+    } 
+    else{
+        console.log(todayDay);
+    }
+    res.render("list",{kinofDay:day});
+
 });
 
 
